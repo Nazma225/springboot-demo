d484c9546e0f6735bf921e9613ec958fd5b2a429
Nazma Nazma <nazma.java@gmail.com>
2022-05-26 10:29:40 -0500

    Updated Rest api

--- src/main/java/com/training/basic/springbootdemo/SpringbootDemoApplication.java
+++ src/main/java/com/training/basic/springbootdemo/SpringbootDemoApplication.java
@@ -11,4 +11,5 @@ public class SpringbootDemoApplication {
 		SpringApplication.run(SpringbootDemoApplication.class, args);
 	}
 
+	
 }
--- src/main/java/com/training/basic/springbootdemo/controller/DemoController.java
+++ src/main/java/com/training/basic/springbootdemo/controller/DemoController.java
@@ -12,9 +12,15 @@ public class DemoController {
 	@Value("${my.name.is}")
 	private String name;
 	
+	@Value("${my.address.is}")
+	private String address;
+	
+	@Value("${my.tech.is}")
+	private String technology;
+	
 	@GetMapping
 	public String sayHello() {
-		return "Hello "+name+"!!";
+		return "Hello "+name+"!!"+", your address is:"+address+"and you work on "+technology;
 	}
 
 }
